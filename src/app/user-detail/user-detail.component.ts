fa4d59340c59ac062b4e644908b9af0ac7f5e99b
Bahaaeddine <bahaaeddine.ghezz@gmail.com>
2023-09-06 14:06:12 +0200

    Input user- detail

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user-detail',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-detail.component.css']
 })
 export class UserDetailComponent {
+
+  @Input() user: any;
 
 }
